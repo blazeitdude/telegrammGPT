663b11b8e35a13bb83e9b443d78d412138817a6d
ldione <slepkinv@mail.ru>
2024-06-09 21:53:50 +0300

    fixed logging when reading the application configuration

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"github.com/spf13/viper"
+	"log"
 	"os"
 	"path/filepath"
 	"telegrammGPT/pkg/botLogger"
@@ -17,29 +18,29 @@ type Configuration struct {
 
 func ReadConfig() Configuration {
 	var conf Configuration
-	log := botLogger.GetLogger()
 	viper.SetConfigName("startUp")
 	viper.SetConfigType("yml")
 	viper.AddConfigPath(filepath.Join("config"))
 	if err := viper.ReadInConfig(); err != nil {
 		switch err.(type) {
 		case viper.ConfigParseError:
-			log.Logger.Fatal("Failed to parse config file")
+			log.Fatal("Failed to parse config file")
 		case viper.ConfigFileNotFoundError:
-			log.Logger.Fatal("Config file not found")
+			log.Fatal("Config file not found")
 		case viper.ConfigMarshalError:
-			log.Logger.Fatal("Failed to marshall config file")
+			log.Fatal("Failed to marshall config file")
 		case viper.UnsupportedConfigError:
-			log.Logger.Fatal("This config file is unsupported")
+			log.Fatal("This config file is unsupported")
 		default:
-			log.Logger.Fatal("unexpected error while reading the configuration file")
+			log.Fatal("unexpected error while reading the configuration file")
 		}
 	}
 	conf.BotConfig.ApiKey = os.Getenv("TELEGRAMM_APIKEY")
 	conf.GptConfig.ApiKey = os.Getenv("GPT_APIKEY")
 
 	if err := viper.Unmarshal(&conf); err != nil {
-		log.Logger.Fatalf("Failed to unmarshall config file")
+		log.Fatal("Failed to unmarshall config file")
+
 	}
 	return conf
 }
